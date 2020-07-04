660b6ca18e8ad05a0f0f49fe1b9159f77de8fe7f
Adam Zofware <adam@zofware.com>
2020-07-03 19:51:51 -0700

    Preserve build number query string when following internal links.

--- docs/js/build-dependent-content.js
+++ docs/js/build-dependent-content.js
@@ -2,6 +2,9 @@
 layout: javascript
 ---
 
+// how to inject liquid tags into javascript:
+// https://stackoverflow.com/questions/39853245/reach-jekyll-variables-with-javascript-and-pass-it-throught-dom-manipulation
+
 // borrowed from https://jennamolby.com/how-to-display-dynamic-content-on-a-page-using-url-parameters/
 
 // Parse the URL parameter
@@ -17,12 +20,15 @@ function getParameterByName(name, url) {
 
 // Give the parameter a variable name
 var buildNumber = getParameterByName('build');
+var hasExplicitBuildNumber = true;
 if (buildNumber == null || buildNumber == '') {
     buildNumber = {{ site.data.settings.basic_settings.current_build }};
+    hasExplicitBuildNumber = false;
 }
 
-// https://www.sitepoint.com/jquery-document-ready-plain-javascript/
-document.addEventListener("DOMContentLoaded", function () {
+jQuery(document).ready(function () {
+
+    // update visibility of content based on the current build number
     var elements = document.getElementsByClassName('build-dependent-content');
     for (var i = 0; i < elements.length; i++) {
 
@@ -30,5 +36,17 @@ document.addEventListener("DOMContentLoaded", function () {
             (!("maxBuild" in elements[i].dataset) || (buildNumber <= elements[i].dataset.maxBuild))) {
                 elements[i].style.display = 'block';
         }
+    }
+
+    // update outgoing links to include the build number
+    // https://stackoverflow.com/questions/23035045/need-to-make-a-url-query-string-persist-across-a-link
+    if (hasExplicitBuildNumber) {
+        $('a[href]').each(function () {
+            var elem = $(this);
+            var href = elem.attr('href');
+            if (href.charAt(0) == "/" || href.startsWith("{{ site.url }}")) {
+                elem.attr('href', href + (href.indexOf('?') != -1 ? '&' : '?') + 'build=' + buildNumber);
+            }
+        });
     }
 });
